dff395d06e6261516088c1f2423d28a3548db2ee
Rico van de Vin <rico.vandevin@oneshoe.nl>
2017-10-02 16:46:55 +0200

    Ported call to removed drupal_set_title().

--- src/Controller/ExtlinkExtraController.php
+++ src/Controller/ExtlinkExtraController.php
@@ -2,12 +2,56 @@
 
 namespace Drupal\extlink_extra\Controller;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ExtlinkExtraController extends ControllerBase {
 
   /**
-   * Render the exit modal.
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * Title callback.
+   *
+   * @return string
+   */
+  public function getTitle() {
+    $config = $this->configFactory->get('extlink_extra.settings');
+    $extlink_token_data = [
+      'extlink' => [
+        'external_url' => $_COOKIE['external_url'],
+        'back_url' => $_COOKIE['back_url'],
+      ],
+    ];
+
+    $page_title = $config->get('extlink_page_title') ?: NULL;
+
+    return token_replace($page_title, $extlink_token_data);
+  }
+
+  /**
+   * Render the markup for the exit modal.
    *
    * @return array
    */
