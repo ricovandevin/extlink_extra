d5348e741a480076e155a6b7f0f2a27accf911b4
finlet <finlet@1243726.no-reply.drupal.org>
2017-10-03 18:21:40 +0200

    Issue #2913509 by ricovandevin: [D8] Port additional settings

--- src/Controller/ExtlinkExtraController.php
+++ src/Controller/ExtlinkExtraController.php
@@ -4,8 +4,11 @@ namespace Drupal\extlink_extra\Controller;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\HtmlResponse;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Utility\Token;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class ExtlinkExtraController extends ControllerBase {
 
@@ -24,11 +27,27 @@ class ExtlinkExtraController extends ControllerBase {
   protected $token;
 
   /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * The renderer service.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
    * {@inheritdoc}
    */
-  public function __construct(ConfigFactoryInterface $config_factory, Token $token) {
+  public function __construct(ConfigFactoryInterface $config_factory, Token $token, RequestStack $request_stack, RendererInterface $renderer) {
     $this->configFactory = $config_factory;
     $this->token = $token;
+    $this->requestStack = $request_stack;
+    $this->renderer = $renderer;
   }
 
   /**
@@ -37,7 +56,9 @@ class ExtlinkExtraController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('config.factory'),
-      $container->get('token')
+      $container->get('token'),
+      $container->get('request_stack'),
+      $container->get('renderer')
     );
   }
 
@@ -73,8 +94,15 @@ class ExtlinkExtraController extends ControllerBase {
   public function leave() {
     // Just return the template. Variables will be inserted
     // in extlink_extra_preprocess_extlink_extra_leaving().
-    return [
+    $output = [
       '#theme' => 'extlink_extra_leaving',
     ];
+
+    if ($this->requestStack->getCurrentRequest()->request->get('js')) {
+      $html = $this->renderer->render($output);
+      return new HtmlResponse($html);
+    }
+
+    return $output;
   }
 }
